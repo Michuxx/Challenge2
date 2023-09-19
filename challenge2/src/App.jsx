97c571e7b3abc8914f6fdaaf150c1c8870b15f5d
Michuxx <104158118+Michuxx@users.noreply.github.com>
2023-09-19 18:34:23 +0200

    some css

--- challenge2/src/App.jsx
+++ challenge2/src/App.jsx
@@ -1,33 +1,60 @@
-import './App.css';
-import React from 'react';
-import CardFront from './images/bg-card-front.png';
-import CardBack from './images/bg-card-back.png';
+import "./App.css";
+import React from "react";
+import CardFront from "./images/bg-card-front.png";
+import CardBack from "./images/bg-card-back.png";
 
-function App(){
+function App() {
   return (
-      <div className="container">
-        <div className='credit-site'>
-          <div className="credits-images-side">
-            <div className='front-card'>
+    <div className="container">
+      <div className="credit-site">
+        <div className="credits-images-side">
+          <div className="front-card">
             <img src={CardFront} alt="" />
-            </div>
-            <div className='back-card'>
-              <img src={CardBack} alt="" />
-            </div>
           </div>
-          <div className="user-form-side">
-              <p>CARDHOLDER NAME</p>
-              <input type="text" name="" className="" /> 
+          <div className="back-card">
+            <img src={CardBack} alt="" />
+          </div>
+        </div>
+        <div className="user-form-side">
+          <div className="user-form">
+            <div className="card-name">
+              <label>CARDHOLDER NAME</label>
               <br />
-              <p>CARD NUMBER</p>
-              <input type="text" name="" className="" />
-              <br /><br />
-              <input type="text" name="" id="" size="2"/>
-              
+              <input type="text" name="" className="card-input" />
+            </div>
+            <br />
+            <div className="card-number">
+              <label>CARD NUMBER</label>
+              <br />
+              <input type="text" name="" className="card-input" />
+            </div>
+            <br />
+            <br />
+            <div className="expiration">
+              <div className="card-expiration">
+                <label>EXP. DATE (MM/YY)</label>
+                <br />
+                <input type="text" name="" className="card-input" size="2" />
+                <input
+                  type="text"
+                  name=""
+                  className="card-input"
+                  size="2"
+                  id="card-input-id"
+                />
+              </div>
+              <div className="card-three-numbers">
+                <label>CVC</label>
+                <br />
+                <input type="text" name="" className="card-input" />
+              </div>
+            </div>
+            <button className="confirm-btn">Confirm</button>
           </div>
         </div>
       </div>
+    </div>
   );
-  }
+}
 
-export default App
+export default App;
