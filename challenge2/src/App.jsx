fdf566cf03ce74a23596fe710435cbcde6c380bd
Michuxx <104158118+Michuxx@users.noreply.github.com>
2023-09-19 16:24:43 +0200

    added some divs and inputs

--- challenge2/src/App.jsx
+++ challenge2/src/App.jsx
@@ -6,15 +6,26 @@ import CardBack from './images/bg-card-back.png';
 function App(){
   return (
       <div className="container">
-        <div className="credits-images-side">
-          <div className='front-card'>
-           <img src={CardFront} alt="" />
+        <div className='credit-site'>
+          <div className="credits-images-side">
+            <div className='front-card'>
+            <img src={CardFront} alt="" />
+            </div>
+            <div className='back-card'>
+              <img src={CardBack} alt="" />
+            </div>
           </div>
-          <div className='back-card'>
-            <img src={CardBack} alt="" />
+          <div className="user-form-side">
+              <p>CARDHOLDER NAME</p>
+              <input type="text" name="" className="" /> 
+              <br />
+              <p>CARD NUMBER</p>
+              <input type="text" name="" className="" />
+              <br /><br />
+              <input type="text" name="" id="" size="2"/>
+              
           </div>
         </div>
-        <div className="user-form-side"></div>
       </div>
   );
   }
