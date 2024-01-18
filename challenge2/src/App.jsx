03116292942811ff785279082504926adff6f4b9
Michuxx <104158118+Michuxx@users.noreply.github.com>
2024-01-18 17:09:24 +0100

    css of rensposive mode

--- challenge2/src/App.jsx
+++ challenge2/src/App.jsx
@@ -3,71 +3,69 @@ import React from "react";
 
 function App() {
   return (
-    <div className="container">
-      <div className="credit-site">
-        <div className="credits-images-side">
-          <div className="front-card">
-            <div className="front-card-content">
-              <div className="circles">
-                <div className="big-circle"></div>
-                <div className="small-circle"></div>
-              </div>
-              <div className="front-card-number-display">
-                <p>1234 1234 1234 1234</p>
-              </div>
-              <div className="bottom-front-card-information-display">
-                <p>Strażak Braum</p>
-                <p>12/12</p>
-              </div>
+    <div className="credit-site">
+      <div className="credits-images-side">
+        <div className="front-card">
+          <div className="front-card-content">
+            <div className="circles">
+              <div className="big-circle"></div>
+              <div className="small-circle"></div>
             </div>
-          </div>
-          <div className="back-card">
-            <div className="back-card-content">
-              <p>000</p>
+            <div className="front-card-number-display">
+              <p>1234 1234 1234 1234</p>
+            </div>
+            <div className="bottom-front-card-information-display">
+              <p>Strażak Braum</p>
+              <p>12/12</p>
             </div>
           </div>
         </div>
-        <div className="user-form-side">
-          <div className="user-form">
-            <div className="card-name">
-              <label>CARDHOLDER NAME</label>
-              <br />
-              <input type="text" name="" className="card-input" />
-            </div>
-            <br />
-            <div className="card-number">
-              <label>CARD NUMBER</label>
-              <br />
-              <input type="text" name="" className="card-input" />
-            </div>
-            <br />
-            <br />
-            <div className="expiration">
-              <div className="card-expiration">
-                <div className="card-expiration-label">
-                  <label>EXP. DATE (MM/YY)</label>
-                </div>
-                <div className="card-expiration-inputs">
-                  <input type="text" name="" className="card-input" />
-                  <input
-                    type="text"
-                    name=""
-                    className="card-input"
-                    id="card-input-id"
-                  />
-                </div>
-              </div>
-              <div className="card-three-numbers">
-                <div className="card-three-numbers-label">
-                  <label>CVC</label>
-                </div>
-                <div className="card-three-numbers-input">
-                  <input type="text" name="" className="card-input" />
-                </div>
-              </div>
-            </div>
-            <button className="confirm-btn">Confirm</button>
+        <div className="back-card">
+          <div className="back-card-content">
+            <p>000</p>
           </div>
+        </div>
+      </div>
+      <div className="user-form-side">
+        <div className="user-form">
+          <div className="card-name">
+            <label>CARDHOLDER NAME</label>
+            <br />
+            <input type="text" name="" className="card-input" />
+          </div>
+          <br />
+          <div className="card-number">
+            <label>CARD NUMBER</label>
+            <br />
+            <input type="text" name="" className="card-input" />
+          </div>
+          <br />
+          <br />
+          <div className="expiration">
+            <div className="card-expiration">
+              <div className="card-expiration-label">
+                <label>EXP. DATE (MM/YY)</label>
+              </div>
+              <div className="card-expiration-inputs">
+                <input type="text" name="" className="card-input" />
+                <input
+                  type="text"
+                  name=""
+                  className="card-input"
+                  id="card-input-id"
+                />
+              </div>
+            </div>
+            <div className="card-three-numbers">
+              <div className="card-three-numbers-label">
+                <label>CVC</label>
+              </div>
+              <div className="card-three-numbers-input">
+                <input type="text" name="" className="card-input" />
+              </div>
+            </div>
+          </div>
+          <button className="confirm-btn">Confirm</button>
         </div>
       </div>
     </div>
